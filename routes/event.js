d9a04ff35e5eb0f1c40ca9095f053de18d71e90a
Marta Noya <marta.noya@est.fib.upc.edu>
2016-10-16 17:46:09 +0200

    api esdeveniments amb GET de tots els esdeveniments - sense comprovar l'usuari ni amb les dades finals però amb persistencia - // + el codi anterior millorat

--- routes/event.js
+++ routes/event.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const _ = require('lodash')
 const router = express.Router()
-const mysql = require('mysql');
+const mysql = require('promise-mysql');
 
 var pool  = mysql.createPool({
   host     : 'localhost',
@@ -13,22 +13,21 @@ var pool  = mysql.createPool({
 
 router
   .post('/', function(req, res, next) {
-    console.log("POST: ", req.body)
+    console.log("POST events ")
     if(!req.body) {
-      console.log("No hi ha body :(")
       res
         .status(403)
         .json({error: true, message: 'Body empty'})
     }
 
     let _event = req.body
-    _event._id = 1 // L'ID es pot fer com AUTOICREMENT però crec que hauríem de guardar l'ID que ens envien de la API que usem.
+    _event.ID = 1 // L'ID es pot fer com AUTOICREMENT però crec que hauríem de guardar l'ID que ens envien de la API que usem.
 
     pool.getConnection(function(err, mysqlConnection) {
       mysqlConnection.query("CREATE TABLE IF NOT EXISTS events(ID int NOT NULL, title varchar(255) NOT NULL, description varchar(2000), PRIMARY KEY (ID));", function(err, result) {
         if (!err) {
           console.log("Table events created: " + JSON.stringify(result));
-          mysqlConnection.query("INSERT INTO events SET ?", {id: _event._id, title: _event.title, description: _event.description}, function(err, result) {
+          mysqlConnection.query("INSERT INTO events SET ?", {ID: _event.ID, title: _event.title, description: _event.description}, function(err, result) {
             if (!err) {
               console.log("Insert event done: " + JSON.stringify(result));
               res
@@ -51,6 +50,26 @@ router
       });
     });
 
+  })
+
+  .get('/', function(req, res, next) {
+    console.log("GET events");
+    pool.getConnection(function(err, mysqlConnection) {
+      mysqlConnection.query("SELECT * FROM events;", function(err, result) {
+        if (!err) {
+          res
+            console.log("Get events done: " + JSON.stringify(result));
+            res
+              .status(200)
+              .json({events: result})
+        } else {
+          console.log("Error GEEEEEET: " + JSON.stringify(err));
+          res
+            .status(500)
+            .json({error: true, message: 'DB error: ' +  JSON.stringify(err)})
+        }
+      });
+    });
   })
 
 module.exports = router
--- test/event.js
+++ test/event.js
@@ -3,7 +3,7 @@ let request = require('supertest-as-promised');
 const _ = require('lodash');
 const api = require('../app');
 const host = api;
-const mysql = require('mysql');
+const mysql = require('promise-mysql');
 
 request = request(host);
 
@@ -18,33 +18,8 @@ describe('route of events', function() {
 
   // Evidentment el DROP TABLE IF EXISTS és temporal i quan tinguem esdeveniments de debó, només borraré els creats en les proves i no tots XD
 
-  beforeEach(function() {
-    pool.getConnection(function(err, mysqlConnection) {
-      mysqlConnection.query("DROP TABLE IF EXISTS events", function(err, result) {
-        if (!err) {
-          console.log("Table events doesn't exist now: " + JSON.stringify(result));
-          mysqlConnection.release();
-        } else {
-          console.log("Error: " + JSON.stringify(err));
-        }
-      });
-    });
-  });
-
-  after(function() { // Al final netejo la BD de les proves que he fet
-    pool.getConnection(function(err, mysqlConnection) {
-      mysqlConnection.query("DROP TABLE IF EXISTS events", function(err, result) {
-        if (!err) {
-          console.log("Table events doesn't exist now: " + JSON.stringify(result));
-          mysqlConnection.release();
-        } else {
-          console.log("Error: " + JSON.stringify(err));
-        }
-      });
-    });
-  });
-
   describe('POST /events', function() {
+
     it('should create an event', function(done) {
       this.timeout(5000); // Per fer proves, però no cal
 
@@ -53,27 +28,95 @@ describe('route of events', function() {
         'description': 'description testing 01'
       }
 
-      request
-        .post('/events')
-        .set('Accept', 'application/json')
-        .send(event)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-      .end((err, res) => {
-        let body = res.body
+      pool.getConnection().then(function(mysqlConnection) {
+        mysqlConnection.query("DROP TABLE IF EXISTS events")
+        .then((res) => {
+          console.log("Table events doesn't exist now: " + JSON.stringify(res));
+        })
+        .catch((err) => {
+          console.log("Error: " + JSON.stringify(err));
+        })
+        .finally(() => {
+          request
+            .post('/events')
+            .set('Accept', 'application/json')
+            .send(event)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+          .end((err, res) => {
+            let body = res.body
 
-        expect(body).to.have.property('event')
-        event = body.event
+            expect(body).to.have.property('event')
+            event = body.event
 
-        expect(event).to.have.property('title', 'title testing 01')
-        expect(event).to.have.property('description', 'description testing 01')
-        expect(event).to.have.property('_id')
+            expect(event).to.have.property('title', 'title testing 01')
+            expect(event).to.have.property('description', 'description testing 01')
+            expect(event).to.have.property('ID')
 
-        done(err)
-      })
+            done(err)
+          })
+        });
+      });
     })
+
+
   })
 
+  describe('GET /events', function() {
 
+    it('should obtain all the events', function(done) {
+
+      this.timeout(5000); // Per fer proves
+
+      pool.getConnection().then(function(mysqlConnection) {
+        mysqlConnection.query("DROP TABLE IF EXISTS events")
+        .then((res) => {
+          console.log("Table events doesn't exist now: " + JSON.stringify(res));
+          return mysqlConnection.query("CREATE TABLE events(ID int NOT NULL, title varchar(255) NOT NULL, description varchar(2000), PRIMARY KEY (ID));")
+        })
+        .then((res) => {
+          console.log("Table events created: " + JSON.stringify(res));
+          return mysqlConnection.query("INSERT INTO events SET ?", {id: 2, title: "Títol 01", description: "Descripció random"})
+        })
+        .then((res) => {
+          console.log("Insert event1 done: " + JSON.stringify(res));
+          return mysqlConnection.query("INSERT INTO events SET ?", {id: 3, title: "Títol 02", description: "Descripció random"})
+        })
+        .then((res) => {
+          console.log("Insert event2 done: " + JSON.stringify(res));
+        })
+        .catch((err) => {
+          console.log("Error: " + JSON.stringify(err));
+        })
+        .finally(() => {
+          request
+            .get('/events')
+            .set('Accept', 'application/json')
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+          .then((res) => {
+            let body = res.body
+
+            expect(body).to.have.property('events')
+            expect(body.events).to.be.an('array');
+            expect(Object.keys(body.events).length).to.eql(2);
+
+            let events = body.events;
+            console.log("BODY.EVENTS: " + JSON.stringify(events));
+
+            expect(events[0]).to.have.property('ID', 2)
+            expect(events[0]).to.have.property('title', 'Títol 01')
+            expect(events[0]).to.have.property('description', 'Descripció random')
+
+            expect(events[1]).to.have.property('ID', 3)
+            expect(events[1]).to.have.property('title', 'Títol 02')
+            expect(events[1]).to.have.property('description', 'Descripció random')
+            done();
+          }, done)
+        });
+      });
+
+    });
+  });
 
 });
