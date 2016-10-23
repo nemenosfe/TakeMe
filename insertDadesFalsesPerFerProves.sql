b457c522d4bef2c94f70410c0b532629b339121f
Marta Noya <marta.noya@est.fib.upc.edu>
2016-10-23 17:42:07 +0200

    sql amb unes poques dades falses per poder fer proves

---
+++ insertDadesFalsesPerFerProves.sql
@@ -0,0 +1,13 @@
+INSERT INTO achievements VALUES ("logro 01", "random description 01");
+INSERT INTO achievements VALUES ("logro 02", "random description 02");
+INSERT INTO achievements VALUES ("logro 03", "random description 03");
+
+INSERT INTO rewards VALUES ("recompensa 01", "Descripció de la primera recompensa", 100, 1);
+INSERT INTO rewards VALUES ("recompensa 02", "Descripció de la segona recompensa", 200, 1);
+INSERT INTO rewards VALUES ("recompensa 02", "Descripció de la tercera recompensa", 50, 2);
+
+INSERT INTO users VALUES (1, "provider", "Nom1", "Cognom1", "emailfals1@gmail.com", 500, 500, 1);
+INSERT INTO users VALUES (2, "provider", "Nom2", "Cognom2", "emailfals2@gmail.com", 200, 200, 1);
+INSERT INTO users VALUES (3, "provider", "Nom3", "Cognom3", "emailfals3@gmail.com", 60, 60, 1);
+
+
