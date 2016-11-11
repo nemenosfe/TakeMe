a635f35d81a0df7dcdd6081767b495e7e7f008e5
Marta Noya <marta.noya@est.fib.upc.edu>
2016-11-11 16:54:23 +0100

    nou insert per les taules modificades

--- insertDadesFalsesPerFerProves.sql
+++ insertDadesFalsesPerFerProves.sql
@@ -1,6 +1,6 @@
-INSERT INTO achievements VALUES ("logro 01", "random description 01");
-INSERT INTO achievements VALUES ("logro 02", "random description 02");
-INSERT INTO achievements VALUES ("logro 03", "random description 03");
+INSERT INTO achievements VALUES (1, "logro 01", "random description 01", 10, "music");
+INSERT INTO achievements VALUES (2, "logro 02", "random description 02", 20, "comedy");
+INSERT INTO achievements VALUES (3, "logro 03", "random description 03", 30, "other");
 
 INSERT INTO rewards VALUES ("recompensa 01", "Descripció de la primera recompensa", 100, 1);
 INSERT INTO rewards VALUES ("recompensa 02", "Descripció de la segona recompensa", 200, 1);
@@ -11,10 +11,10 @@ INSERT INTO users VALUES (1, "provider", "Nom1", "Cognom1", "emailfals1@gmail.co
 INSERT INTO users VALUES (2, "provider", "Nom2", "Cognom2", "emailfals2@gmail.com", 200, 200, 1);
 INSERT INTO users VALUES (3, "provider", "Nom3", "Cognom3", "emailfals3@gmail.com", 60, 60, 1);
 
-INSERT INTO events VALUES ("E0-001-093720767-4", 0, "2017-02-27 00:00:00", "2017-03-02 00:00:00", 0, 10);
-INSERT INTO events VALUES ("E0-001-000278174-6", 0, "2005-08-24 12:00:00", null, 0, 5);
-INSERT INTO events VALUES ("E0-001-096844204-0@2016102500", 0, "2016-10-25 00:35:00", null, 0, 5);
-INSERT INTO events VALUES("E0-001-095872589-2@2016102508", 2, null, null, 0, 10);
+INSERT INTO events VALUES ("E0-001-093720767-4", 0, "2017-02-27 00:00:00", "2017-03-02 00:00:00", 0, 10, "music");
+INSERT INTO events VALUES ("E0-001-000278174-6", 0, "2005-08-24 12:00:00", null, 0, 5, "music");
+INSERT INTO events VALUES ("E0-001-096844204-0@2016102500", 0, "2016-10-25 00:35:00", null, 0, 5, "comedy");
+INSERT INTO events VALUES("E0-001-095872589-2@2016102508", 2, null, null, 0, 10, "other");
 
 INSERT INTO attendances VALUES("E0-001-093720767-4", 1, "provider", false);
 INSERT INTO attendances VALUES("E0-001-000278174-6", 1, "provider", true);
@@ -22,7 +22,7 @@ INSERT INTO attendances VALUES("E0-001-000278174-6", 2, "provider", true);
 INSERT INTO attendances VALUES("E0-001-096844204-0@2016102500", 1, "provider", false);
 INSERT INTO attendances VALUES("E0-001-095872589-2@2016102508", 1, "provider", false);
 
-INSERT INTO acquisitions VALUES(1, "provider", "logro 01");
+INSERT INTO acquisitions VALUES(1, "provider", 1);
 
 INSERT INTO purchases VALUES (1, "provider", "recompensa 01", 3);
 INSERT INTO purchases VALUES (1, "provider", "recompensa 02", 5);
