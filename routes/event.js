747caf4e9d65f24babce5f0cbf9c4e2af1aa5b0b
Marta Noya <marta.noya@est.fib.upc.edu>
2016-12-29 17:05:15 +0100

    Amb alguns console.log per debugging

--- routes/event.js
+++ routes/event.js
@@ -114,12 +114,14 @@ router
         })
         .then((eventResEventful) => {
           let moment = "past";
-          for (let index = 0; index < eventResEventful.length; index++) {
+          for (let index = 0; index < eventResEventful.length; ++index) {
             if (moment != "future") { moment = utilsEventRelated.getMoment(database_result[index].start, database_result[index].all_day, database_result[index].stop); }
             let elementArray = utilsEventRelated.getFinalJSONOfAnEvent(eventResEventful[index], null);
             elementArray.event.checkin_done = database_result[index].checkin_done;
             elementArray.event.number_attendances = database_result[index].number_attendances;
             elementArray.event.takes = database_result[index].takes;
+            console.log(`index: ${index} - length: ${eventResEventful.length}`);
+            console.log(`elementArray: ${JSON.stringify(elementArray)}`);
             eventsResponse[moment]["events"][index] = elementArray;
           }
 
