e5fa561dd69b05e7ceecb452183131d905faaa99
Marta Noya <marta.noya@est.fib.upc.edu>
2017-01-10 15:11:26 +0100

    x 250

--- utils/userRelated.js
+++ utils/userRelated.js
@@ -6,5 +6,5 @@ module.exports = {
     while (experience >= this.getNextLevelExperience(new_level+1)) { ++new_level; }
     return new_level;
   },
-  getNextLevelExperience: function (nextLevel) { return nextLevel * Math.log10(nextLevel) * 100; }
+  getNextLevelExperience: function (nextLevel) { return nextLevel * Math.log10(nextLevel) * 250; }
 };
